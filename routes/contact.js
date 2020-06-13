e894474d1cf4ed8f8fa600bf12b8271d3e83aed5
Jay <thk116@gmail.com>
2020-06-13 01:31:03 -0700

    fixed cors issue and modified email to req.body and pass dynamic data for email

--- routes/contact.js
+++ routes/contact.js
@@ -13,23 +13,35 @@ router.use(express.urlencoded({ extended: false }));
 
 router.use(morgan('dev'));
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.send('okay')
+    res.send('no wey')
 })
 
 router.post('/', (req, res) => {
+    let email = req.body
     const msg = {
-        to: 'thk116@gmail.com',
-        from: 'jthk1106@gmail.com',
-        subject: 'Sending with Twilio SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
+        "personalizations":[{"to":[{"email":"thk116@gmail.com","name":"Grandmaster Jay"}],"subject":"New msg from pfolio!"}],
+        "content": [{"type": "text/plain", "value": email.message}],
+        "from":{"email":email.email,"name":email.name},
+        "reply_to":{"email":email.email,"name":email.name}
+    }
+    // const msg = {
+    //     to: 'thk116@gmail.com',
+    //     from: 'someone',
+    //     subject: 'New message from your pfolio!',
+    //     text: 'Youre hired!',
+    //     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    // };
     sgMail.send(msg)
         .then(res => console.log('the RES: ', res))
         .catch(err => console.log('the ERR: ', err))
-
     res.json('email sent')
 });
 
